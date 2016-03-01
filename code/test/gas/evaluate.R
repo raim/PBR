369f45d23611503483ef0b6619622b525dd85aae
Rainer Machne <raim@tbi.univie.ac.at>
2016-03-01 17:43:18 +0100

    added latest data

--- code/test/gas/evaluate.R
+++ code/test/gas/evaluate.R
@@ -68,3 +68,30 @@ axis(4,col.axis="red",col.ticks="red",col="red")
 mtext("P [ppm]", 4, 1.7, col="red")
 dev.off()
 
+
+##room recording, 20160301, 
+## start at ca. 9:00, stop at 21:00
+dat <- read.delim("~/work/hhu_2015/pbr_hackathon_201503/code/test/gas/data/20160301.txt",sep=" ",header=TRUE,skip=1)
+time <- dat[,1]/1000/60/60 ## time in hours
+time <- time -min(time) + 9
+co2 <- dat[,2] ## CO2 in ppm
+o2 <- 100*dat[,3]/dat[,4] ## O2 in %
+P <- dat[,4] ## total pressure in bar
+T <- dat[,5] ## T in °C
+
+png("~/work/hhu_2015/pbr_hackathon_201503/code/test/gas/data/20160301.png",width=4,height=3,units="in",res=200)
+par(mfcol=c(2,1),mai=c(0.1,.6,0.1,0.6),mgp=c(1.7,.6,0))
+plot(time,co2,ylab="CO2 [ppm]",xlab="time [h]",type="l",ylim=c(0,1400),axes=FALSE)
+axis(1,at=seq(1,24,1));axis(2);axis(3,at=seq(1,24,1),tcl=.5)
+par(new=TRUE)
+plot(time, o2, type="l", ylab=NA,xlab=NA,axes=F,col="red")
+axis(4,col.axis="red",col.ticks="red",col="red")
+mtext("O2 [%]", 4, 1.7, col="red")
+plot(time, T, ylab="T [°C]",xlab="time [h]",type="l",axes=FALSE)
+axis(1,at=seq(1,24,1),tcl=.5);axis(2)
+par(new=TRUE)
+plot(time, P, type="l", ylab=NA,xlab=NA,axes=F,col="red")
+axis(4,col.axis="red",col.ticks="red",col="red")
+mtext("P [ppm]", 4, 1.7, col="red")
+dev.off()
+
