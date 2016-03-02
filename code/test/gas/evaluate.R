479f89c77733d52eac40d38de24db765efd682fd
Rainer Machne <raim@tbi.univie.ac.at>
2016-03-02 18:55:32 +0100

    added code for the last two gasometer test runs

--- code/test/gas/evaluate.R
+++ code/test/gas/evaluate.R
@@ -79,6 +79,7 @@ o2 <- 100*dat[,3]/dat[,4] ## O2 in %
 P <- dat[,4] ## total pressure in bar
 T <- dat[,5] ## T in °C
 
+
 png("~/work/hhu_2015/pbr_hackathon_201503/code/test/gas/data/20160301.png",width=4,height=3,units="in",res=200)
 par(mfcol=c(2,1),mai=c(0.1,.6,0.1,0.6),mgp=c(1.7,.6,0))
 plot(time,co2,ylab="CO2 [ppm]",xlab="time [h]",type="l",ylim=c(0,1400),axes=FALSE)
@@ -95,3 +96,41 @@ axis(4,col.axis="red",col.ticks="red",col="red")
 mtext("P [ppm]", 4, 1.7, col="red")
 dev.off()
 
+##room recording, 20160302, 
+## start at ca. 11:00, stop at 21:00
+dat <- read.delim("~/work/hhu_2015/pbr_hackathon_201503/code/test/gas/data/20160302.txt",sep=" ",header=TRUE,skip=32)
+## rm NA
+dat <- dat[!is.na(dat[,1]),]
+
+time <- dat[,1]/1000/60/60 ## time in hours
+time <- time -min(time,na.rm=TRUE) + 11
+co2 <- dat[,2] ## CO2 in ppm
+o2 <- 100*dat[,3]/dat[,4] ## O2 in %
+P <- dat[,4] ## total pressure in bar
+T <- dat[,5] ## T in °C
+
+## rm extremes
+co2[co2<quantile(co2,.01,na.rm=TRUE)] <- NA
+co2[co2>quantile(co2,.99,na.rm=TRUE)] <- NA
+o2[o2<quantile(o2,.01,na.rm=TRUE)] <- NA
+o2[o2>quantile(o2,.99,na.rm=TRUE)] <- NA
+P[P<quantile(P,.01,na.rm=TRUE)] <- NA
+P[P>quantile(P,.99,na.rm=TRUE)] <- NA
+T[T<quantile(T,.01,na.rm=TRUE)] <- NA
+T[T>quantile(T,.99,na.rm=TRUE)] <- NA
+ 
+png("~/work/hhu_2015/pbr_hackathon_201503/code/test/gas/data/20160302.png",width=4,height=3,units="in",res=200)
+par(mfcol=c(2,1),mai=c(0.1,.6,0.1,0.6),mgp=c(1.7,.6,0))
+plot(time,co2,ylab="CO2 [ppm]",xlab="time [h]",type="l",ylim=c(0,1400),axes=FALSE)
+axis(1,at=seq(1,24,1));axis(2);axis(3,at=seq(1,24,1),tcl=.5)
+par(new=TRUE)
+plot(time, o2, type="l", ylab=NA,xlab=NA,axes=F,col="red")
+axis(4,col.axis="red",col.ticks="red",col="red")
+mtext("O2 [%]", 4, 1.7, col="red")
+plot(time, T, ylab="T [°C]",xlab="time [h]",type="l",axes=FALSE)
+axis(1,at=seq(1,24,1),tcl=.5);axis(2)
+par(new=TRUE)
+plot(time, P, type="l", ylab=NA,xlab=NA,axes=F,col="red")
+axis(4,col.axis="red",col.ticks="red",col="red")
+mtext("P [ppm]", 4, 1.7, col="red")
+dev.off()
