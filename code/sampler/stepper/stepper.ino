df6e80b7c0b2aa23edf9ef79925190c455d7e2bf
Rainer Machne <raim@tbi.univie.ac.at>
2016-03-03 20:00:47 +0100

    playing with stepper speed

--- code/sampler/stepper/stepper.ino
+++ code/sampler/stepper/stepper.ino
@@ -4,7 +4,7 @@
 
 #include <Stepper.h>
 
-const int stepsPerRevolution = 48;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
                           // for your motor
 
 // initialize the stepper library on the motor shield
@@ -20,29 +20,28 @@ const int dirB = 13;
 
 int x = 0;
 void setup() {
-Serial.begin(9600);
-// set the PWM and brake pins so that the direction pins  // can be used to control the motor:
-pinMode(pwmA, OUTPUT);
-pinMode(pwmB, OUTPUT);
-pinMode(brakeA, OUTPUT);
-pinMode(brakeB, OUTPUT);
-digitalWrite(pwmA, HIGH);
-digitalWrite(pwmB, HIGH);
-digitalWrite(brakeA, LOW);
-digitalWrite(brakeB, LOW);
-
-// initialize the serial port:
-Serial.begin(9600);
-// set the motor speed (for multiple steps only):
-myStepper.setSpeed(2);
+  Serial.begin(9600);
+  // set the PWM and brake pins so that the direction pins  
+  // can be used to control the motor:
+  pinMode(pwmA, OUTPUT);
+  pinMode(pwmB, OUTPUT);
+  pinMode(brakeA, OUTPUT);
+  pinMode(brakeB, OUTPUT);
+  digitalWrite(pwmA, HIGH);
+  digitalWrite(pwmB, HIGH);
+  digitalWrite(brakeA, LOW);
+  digitalWrite(brakeB, LOW);
+  
+  // initialize the serial port:
+  Serial.begin(9600);
+  // set the motor speed (for multiple steps only):
+  myStepper.setSpeed(1); // rpm??
 }
 
 
 void loop() {
-
-  myStepper.step(48);
-  myStepper.step(-48);
-
-  delay(2000);
-
+  
+  myStepper.step(1);
+  //myStepper.step(-2);
+  
 }
