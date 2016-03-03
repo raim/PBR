7e37e33d2945f58bf3ec027bf03e2286aca15e94
Rainer Machne <raim@tbi.univie.ac.at>
2016-03-03 20:01:28 +0100

    adding touch screen and pin comments

--- code/liquid/scale/scale.ino
+++ code/liquid/scale/scale.ino
@@ -1,10 +1,16 @@
-// Hx711.DOUT - pin #A1
-// Hx711.SCK - pin #A0
 
+// TOUCHSCREEN
+#include <Time.h> // convert times (day, hours, minutes, seconds)
+#include <UTFT.h> // Display
+#include <UTouch.h> // Touchscreen
+#include <SPI.h> // dunno
+#include <SD.h> // SD Memory Card
+
+// HX711 SCALE
 #include "hx711.h"
-Hx711 scale(A9, A8); // scale on analog pins A8 and A9
+Hx711 scale(A9, A8); // scale on analog pins A8 (SCK) and A9 (DT/DOUT)
 
-// 12V 5000 rpm DC motor/peristaltic pump
+// 12V 5000 rpm DC MOTOR/PERISTALTIC PUMP
 // PIN CONNECTIONS - channel A
 const int pwmA = 3;   // motor speed via PWM pin 3
 const int brakeA = 9; // motor brake  
@@ -17,6 +23,20 @@ void setup() {
   pinMode(dirA, OUTPUT);   //Initiates Motor Channel A pin
   pinMode(brakeA, OUTPUT); //Initiates Brake Channel A pin
 
+ // get weight
+  Serial.print(scale.getGram(), 1);
+  Serial.println(" g");
+
+  // run motor
+  digitalWrite(dirA, HIGH);    //Establishes forward direction of Channel A
+  digitalWrite(brakeA, LOW);   //Disengage the Brake for Channel A
+  analogWrite(pwmA, 500);      //Spins the motor on Channel A at full speed
+  delay(5000);
+ // get weight
+  Serial.print(scale.getGram(), 1);
+  Serial.println(" g");
+  analogWrite(pwmA, 150);      //Spins the motor on Channel A at full speed
+
 }
 
 void loop() {
@@ -25,10 +45,6 @@ void loop() {
   Serial.print(scale.getGram(), 1);
   Serial.println(" g");
 
-  // run motor
-  digitalWrite(dirA, HIGH);    //Establishes forward direction of Channel A
-  digitalWrite(brakeA, LOW);   //Disengage the Brake for Channel A
-  analogWrite(pwmA, 500);      //Spins the motor on Channel A at full speed
 
   //delay(200);
 }
