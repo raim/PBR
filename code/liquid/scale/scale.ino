6b69c6b3490f7bd9c96390001ec1510ad60ce08d
Rainer Machne <raim@tbi.univie.ac.at>
2016-03-03 18:05:58 +0100

    added DC motor pump control

--- code/liquid/scale/scale.ino
+++ code/liquid/scale/scale.ino
@@ -2,19 +2,33 @@
 // Hx711.SCK - pin #A0
 
 #include "hx711.h"
+Hx711 scale(A9, A8); // scale on analog pins A8 and A9
 
-Hx711 scale(A9, A8);
+// 12V 5000 rpm DC motor/peristaltic pump
+// PIN CONNECTIONS - channel A
+const int pwmA = 3;   // motor speed via PWM pin 3
+const int brakeA = 9; // motor brake  
+const int dirA = 12;  // motor direction
 
 void setup() {
 
   Serial.begin(9600);
+  //Setup Channel A
+  pinMode(dirA, OUTPUT);   //Initiates Motor Channel A pin
+  pinMode(brakeA, OUTPUT); //Initiates Brake Channel A pin
 
 }
 
 void loop() {
 
+  // get weight
   Serial.print(scale.getGram(), 1);
   Serial.println(" g");
 
-  delay(200);
+  // run motor
+  digitalWrite(dirA, HIGH);    //Establishes forward direction of Channel A
+  digitalWrite(brakeA, LOW);   //Disengage the Brake for Channel A
+  analogWrite(pwmA, 500);      //Spins the motor on Channel A at full speed
+
+  //delay(200);
 }
