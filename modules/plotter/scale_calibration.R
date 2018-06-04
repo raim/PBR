07fec84c70593a0671f5215ad6d2fe75f7c51f61
raim <raim@tbi.univie.ac.at>
2018-06-04 16:00:18 +0200

    fixed old scale calibration data/script

--- modules/plotter/scale_calibration.R
+++ modules/plotter/scale_calibration.R
@@ -1,5 +1,10 @@
 
-dat <- read.csv("scale_calibration.txt", sep=";", skip=1, header=TRUE)
+dat <- read.csv("scale_calibration.txt", sep=";",  header=TRUE, comment.char = "#")
+
+calibrate <- dat[12:nrow(dat),]
+dat <- dat[1:11,]
+
+plot(dat,type="b")
 
 dmL <- mean(diff(dat[,"mL"]))
 dg <-  mean(diff(dat[,"g"]))
@@ -8,3 +13,6 @@ sd <- sd(diff(dat[,"g"]))
 slope <- dmL/dg
 
 cat(paste("CORRECTION FACTOR:", slope, "\n"))
+
+plot(calibrate)
+abline(a=0,b=1)
